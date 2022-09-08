3b4cb35c651a393b24e28588ebbc82455ff9afbe
larsensong <larsenmulamba@gmail.com>
2022-09-08 00:20:35 -0700

    betty error fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * return: produce 0 after printing the function
  */
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
