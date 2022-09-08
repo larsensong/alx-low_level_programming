d6c00728210b4496ba89b14660ef42ccad9a747e
larsensong <larsenmulamba@gmail.com>
2022-09-08 00:34:40 -0700

    using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: Always return 0 as sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
