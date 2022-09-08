08f65ebb14b874671039369e06c289391f23b7ac
larsensong <larsenmulamba@gmail.com>
2022-09-08 00:18:24 -0700

    fist c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+ * main -satrt of the code
+ *
+ * return: produce 0 after printing the function
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+{puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
