edeb9027dbe6e049f0efab1083a304bfb1a45bcf
larsensong <larsenmulamba@gmail.com>
2022-09-07 23:12:49 -0700

    c program that prints and returns value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point of the code
+ *
+ * Return: return 0 after printing the said function
+ * /
+ int main(void)
+ {puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+	}
