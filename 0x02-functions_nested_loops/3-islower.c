b435ed827cb45a71b72aab9772884fe341390b0d
larsensong <larsenmulamba@gmail.com>
2022-09-14 12:28:37 -0700

    fix error lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _is lower - function that checks for lower case character
- * &c:	is the int that will use for the argument of function
+ * _islower - function that checks for lower case character
+ * &c: is the int that will use for the argument of function
  * Return: 0
  */
 int _islower(int c)
