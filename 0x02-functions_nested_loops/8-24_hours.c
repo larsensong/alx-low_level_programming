798919347639b2b289e51ff69f14493ec1c211ff
larsensong <larsenmulamba@gmail.com>
2022-09-14 13:31:09 -0700

    fix error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,17 +12,17 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59)
-		{
-			mins_remainder = minutes % 10;
-			hours_remainder = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0');
-			_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
-			minutes++;
-			_putchar('\n');
+	while (minutes <= 59)
+	{
+		mins_remainder = minutes % 10; 
+		hours_remainder = hours % 10;
+		_putchar(hours / 10 + '0');
+		_putchar(hours_remainder + '0');
+		_putchar(':');
+		_putchar(minutes / 10 + '0');
+		_putchar(mins_remainder + '0');
+		minutes++;
+		_putchar('\n');
 		}
 		hours++;
 		minutes = 0;
