90c66719a6fd962523db9ad73879aa60bf07093a
larsensong <larsenmulamba@gmail.com>
2022-09-14 13:40:23 -0700

    last attempt

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,17 +14,17 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{
-		mins_remainder = minutes % 10; 
-		hours_remainder = hours % 10;
-		_putchar(hours / 10 + '0');
-		_putchar(hours_remainder + '0');
-		_putchar(':');
-		_putchar(minutes / 10 + '0');
-		_putchar(mins_remainder + '0');
-		minutes++;
-		_putchar('\n');
+	mins_remainder = minutes % 10; 
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0');
+	minutes++;
+	_putchar('\n');
 	}
-		hours++;
-		minutes = 0;
-	}
+	hours++;
+	minutes = 0;
+}
 }
