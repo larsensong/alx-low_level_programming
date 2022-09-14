4e250988de0297670450ddac8ff502bae019f04d
larsensong <larsenmulamba@gmail.com>
2022-09-14 14:02:00 -0700

    fix main header errorr

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-int jack_bauer(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
