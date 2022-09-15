5c6cacff9a545e004b051a7c0ae39cecdb4daabf
larsensong <larsenmulamba@gmail.com>
2022-09-15 12:38:37 -0700

    print no 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints no
+ * return: o
+ */
+
+void print_numbers(void)
+{
+int n;
+
+for (n =48; n < 58; n++)
+{
+putchar(n);
+}
+putchar(10);
+}
