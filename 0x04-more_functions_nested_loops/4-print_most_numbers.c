e99abbf0952b4fd4bb6b4501412cc3882463f907
larsensong <larsenmulamba@gmail.com>
2022-09-15 12:55:43 -0700

    print no expect 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * prints_most_numbers - prints no expet 2 and 4
+ *
+ * Return: returns 0
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		putchar(n);
+
+	}
+	putchar(10);
+}
