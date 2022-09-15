4cca9de38d6484d93fda131d8288769e7cb8d1b0
larsensong <larsenmulamba@gmail.com>
2022-09-15 13:15:32 -0700

    print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - printa straight line
+ * @n: parameter value
+ * Return: no value
+ */
+
+void print_line(int n)
+{
+while (n-- > 0)
+{
+Putchar('-');
+}
+putchar('\n');
+}
