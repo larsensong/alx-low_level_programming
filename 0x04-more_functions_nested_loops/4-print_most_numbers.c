ceaeaf7560fd6807aee494de34488e464f95ed8a
larsensong <larsenmulamba@gmail.com>
2022-09-15 22:18:09 -0700

    final

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,16 +9,17 @@
 
 void print_most_numbers(void)
 {
-	int n;
+int n;
 
-	for (n = 48; n < 58; n++)
-	{
-		if((n == 50) || (n == 52))
-		{
-			continue;
-		}
-		putchar(n);
-
-	}
-	putchar(10);
+for (n = 48; n < 58; n++)
+{
+if (n != 50)
+{
+if (n != 52)
+{
+_putchar(n);
+}
+}
+}
+_putchar('\n');
 }
