dbccfe440803bc8450ed84c9cdd1866caa8bbd76
larsensong <larsenmulamba@gmail.com>
2022-09-16 00:05:33 -0700

    lets make  some pythagoras

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: dimensions
+ */
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j < size; j++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
