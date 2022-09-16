b56c13ce629061ba819a608343338a47c33568f7
larsensong <larsenmulamba@gmail.com>
2022-09-16 00:18:42 -0700

    The greatest prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - finds and prints the largest prime no
+ * Return: Always 0 success
+ */
+int main(void)
+{
+long int n;
+long int max;
+lomg int i;
+
+n = 612852475143;
+max = -1;
+
+while (n % 2 == 0)
+{
+max = 2;
+n /= 2;
+}
+for (i = 3; i <= sqrt(n); i = i = 2)
+{
+while (n % i == 0)
+{
+max = i;
+n = n / i;
+}
+}
+if (n > 2)
+max = n;
+printf("%1d\n", max);
+return (0);
+}
