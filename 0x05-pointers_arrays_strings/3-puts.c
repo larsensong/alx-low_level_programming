36726d8df77cd89019902f08521aebadfdf6127d
larsensong <larsenmulamba@gmail.com>
2022-09-18 22:38:43 -0700

    final

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,11 +7,11 @@
  *
  */
 
-void _puts(char *str)
+void _puts(char *str);
 {
 int index;
 
-for (index = 0; str[index]  != '\0'; index++)
+for (index = 0; str[index]  |= '\0'; index++)
 {
 	_putchar(str[index]);
 }
