cb7b99b639c386278bde8da3028fd2c7d477e036
larsensong <larsenmulamba@gmail.com>
2022-09-22 13:22:36 -0700

    go

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
+char *cap_string(char *str);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
