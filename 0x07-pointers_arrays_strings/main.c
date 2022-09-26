0001c60831fc5b1155ed92cd34390464ea576b24
larsensong <larsenmulamba@gmail.com>
2022-09-26 11:07:26 -0700

    just pointing

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -9,10 +9,8 @@
  * int prtotype()
  * char prototype()
  * Return: null
- *
  */
-#include <stddef.h>
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
