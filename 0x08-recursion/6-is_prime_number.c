cd2d4612ffc570ec68a30e990899847af11bcd58
larsensong <larsenmulamba@gmail.com>
2022-09-28 02:07:08 -0700

    function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * helperFunction - returnns 0 or 1
+ * @num: no being checked
+ * @i: possible factor of no
+ *
+ * Return: 0 if no prime
+ */
+int helperFunction(int num,  int i)
+{
+if (i < num)
+{
+if (num % 1 == 0)
+{
+return (0);
+}
+else
+{
+	return (helperFunction(num, i + 1));
+}
+}
+else
+{
+return (1);
+}
+}
+/**
+ * is_prime_number - checks if no is prime or not
+ * @n: no to check
+ *
+ * Return: 1 if is prime
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+{
+return (0);
+}
+else
+{
+	return (helperFunction(n, 2));
+}
+}
