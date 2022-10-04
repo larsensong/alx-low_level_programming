34d930ac7f1c1fb2882722c493986d0373f44643
larsensong <larsenmulamba@gmail.com>
2022-10-04 11:08:23 -0700

    just decided to concatenate allcommits

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * argstostr - print arguments
+ * @ac: take width of the grid
+ * @av: height of grid
+ * Return: the arg line by line
+ */
+char *argstostr(int ac, char **av)
+{
+char *str;
+int count = 0, a = 0, b = 0, c = 0;
+if (ac == 0 || av == NULL)
+return (NULL);
+while (a < ac)
+{
+b = 0;
+while (av[a][b] != '\0')
+{
+count++;
+b++;
+}
+a++;
+}
+count = count + ac + 1;
+str = malloc(sizeof(char) * count);
+if (str == NULL)
+{
+return (NULL);
+}
+for (a = 0; a < ac; a++)
+{
+for (b = 0; av[a][b] != '\0'; b++)
+{
+str[c] = av[a][b];
+c++;
+}
+str[c] = '\n';
+c++;
+}
+return (str);
+}
