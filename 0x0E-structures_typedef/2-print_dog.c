4d99df7e80f164dd2b30ee05a3304ffd255838d2
larsensong <larsenmulamba@gmail.com>
2022-10-11 10:41:26 -0700

    dog creation aint no joke

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,9 @@ void print_dog(struct dog *d)
 if (d == NULL)
 	return;
 if (d->name == NULL)
-	printf("name: (nil)\n");
+	printf("Name: (nil)\n");
 else
-	printf("name: %s\n", d->name);
+	printf("Name: %s\n", d->name);
 if (d->age < 0)
 	printf("Age: (nil)\n");
 else
