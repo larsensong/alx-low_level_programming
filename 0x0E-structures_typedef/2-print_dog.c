a35710c4fa52776a8c7abf2841b62cb3b77d36cd
larsensong <larsenmulamba@gmail.com>
2022-10-11 10:37:57 -0700

    crating the structure of the dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - print a struct dog
+ * @d: struct dog to be printed
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+	return;
+if (d->name == NULL)
+	printf("name: (nil)\n");
+else
+	printf("name: %s\n", d->name);
+if (d->age < 0)
+	printf("Age: (nil)\n");
+else
+	printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+	printf("Owner: (nil)\n");
+else
+	printf("owner: %s\n", d->owner);
+}
