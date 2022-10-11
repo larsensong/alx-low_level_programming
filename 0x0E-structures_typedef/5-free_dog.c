dcd6f61280829aa006ca3c6c3f254a289c54e7e4
larsensong <larsenmulamba@gmail.com>
2022-10-11 11:44:46 -0700

    rex free into the world

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - free dog
+ * @d: The dog to free
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+	return;
+free(d->owner);
+free(d->name);
+free(d);
+}
