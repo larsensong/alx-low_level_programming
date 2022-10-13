04485394b239a75cb162bd34fd157979c8a4aa1c
larsensong <larsenmulamba@gmail.com>
2022-10-13 12:03:04 -0700

    program that performs simple operations add, mut, sub,div,mod

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - calculate sum of 2 int
+ * @a: first
+ * @b: second int
+ * Return: sum of a nd b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - calculate diff of 2 int
+ * @a: fist
+ * @b: second
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - calculate product of int
+ * @a: first int
+ * @b: second int
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - calculate div
+ * @a: first
+ * @b: second
+ * Return: result of division
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - calc remainder of division modulus
+ * @a: first
+ * @b: second
+ * Return: remainder of div
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
