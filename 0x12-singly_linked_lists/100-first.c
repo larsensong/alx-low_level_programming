51b121e61eb10b7e80a37f51a6975e3c7281007f
larsensong <larsenmulamba@gmail.com>
2022-10-24 11:18:45 -0700

    function that prints before its executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - prints a string
+ *
+ */
+void hare(void)
+{
+printf("You're beat! and yet, you must allow,"
+	"\nI bore my house upon my back!\n");
+}
