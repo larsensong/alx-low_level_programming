8e78ddb624de162f4182e14b84836b3d7bf9a39e
larsensong <larsenmulamba@gmail.com>
2022-11-01 23:24:00 -0700

    a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * read_textfile - reads a text and prints a postfix text
+ * @filename: pointer to the name of the file
+ * @letters: no of letters the func prints
+ * Return: if func fails then fileneme is null
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t o, r, w;
+char *buffer;
+if (filename == NULL)
+	return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+	return (0);
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
+
+if (o == -1 || r == -1 || w != r)
+{
+	free(buffer);
+	return (0);
+}
+free(buffer);
+close(o);
+return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+My file decriptors readme file in c
